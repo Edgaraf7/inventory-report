c05208ba03fadf98beb4e0029f94b7ff72047f9c
edgar <edgar.dev22@gmail.com>
2024-05-10 09:21:12 -0300

    Chore: add importers.py/JsonImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,7 +1,7 @@
 from typing import Dict, Type, List
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
-
+import json
 
 class Importer(ABC):
     def __init__(self, path: str):
@@ -12,10 +12,30 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
+        
+        products = []
+        for item in data:
+            product = Product(
+                id=item["id"],
+                product_name=item["product_name"],
+                company_name=item["company_name"],
+                manufacturing_date=item["manufacturing_date"],
+                expiration_date=item["expiration_date"],
+                serial_number=item["serial_number"],
+                storage_instructions=item["storage_instructions"]
+            )
+            products.append(product)
+        
+        return products
+
+
+class CsvImporter(Importer):
     pass
-class CsvImporter:
-    pass
+
 # Não altere a variável abaixo
 IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
