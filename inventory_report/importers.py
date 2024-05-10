fe4549b60a1e0a9bcf94e71fa230a17fc574c9c8
edgar <edgar.dev22@gmail.com>
2024-05-10 09:47:34 -0300

    chora: add CsvImporter/importers.py

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -2,6 +2,7 @@ from typing import Dict, Type, List
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
 import json
+import csv
 
 class Importer(ABC):
     def __init__(self, path: str):
@@ -34,7 +35,25 @@ class JsonImporter(Importer):
 
 
 class CsvImporter(Importer):
-    pass
+    def import_data(self) -> List[Product]:
+        products = []
+
+        with open(self.path, "r", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                product = Product(
+                    id=row["id"],
+                    product_name=row["product_name"],
+                    company_name=row["company_name"],
+                    manufacturing_date=row["manufacturing_date"],
+                    expiration_date=row["expiration_date"],
+                    serial_number=row["serial_number"],
+                    storage_instructions=row["storage_instructions"]
+                )
+                products.append(product)
+        
+        return products
+
 
 # Não altere a variável abaixo
 IMPORTERS: Dict[str, Type[Importer]] = {
