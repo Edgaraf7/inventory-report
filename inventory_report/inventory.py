39dd67e5e821c794bfa98f413a8e49ecf8668f6a
edgar <edgar.dev22@gmail.com>
2024-05-10 09:25:48 -0300

    chore: inventory.py

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,13 @@
+from typing import List
+from inventory_report.product import Product
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] = None):
+        self._data = [] if data is None else data
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, products: List[Product]):
+        self._data.extend(products)
