80cf5a3da4c31a661be9fe29d0280f8b4c78a034
edgar <edgar.dev22@gmail.com>
2024-05-10 09:56:35 -0300

    fix: complete_report.py

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,25 +1,24 @@
-from typing import List
 from inventory_report.reports.simple_report import SimpleReport
+
 
 class CompleteReport(SimpleReport):
     def generate(self) -> str:
-        # Utiliza o método generate da classe SimpleReport para obter as informações básicas do relatório
-        report = super().generate()
+        simple_report = super().generate()
+        company_inventory = self._get_company_inventory()
 
-        # Calcula a quantidade de produtos estocados por empresa
-        stocked_products_by_company = {}
+        complete_report = (f"{simple_report}\nStocked products by company:\n")
+        for company, count in company_inventory.items():
+            complete_report += f"- {company}: {count}\n"
+
+        return complete_report
+
+    def _get_company_inventory(self) -> dict:
+        company_inventory = {}
         for inventory in self.inventories:
             for product in inventory.data:
                 company = product.company_name
-                if company in stocked_products_by_company:
-                    stocked_products_by_company[company] += 1
+                if company in company_inventory:
+                    company_inventory[company] += 1
                 else:
-                    stocked_products_by_company[company] = 1
-        
-        # Formata a lista de empresas e a quantidade de produtos estocados por empresa
-        company_list = "\n".join([f"- {company}: {count}" for company, count in stocked_products_by_company.items()])
-
-        # Adiciona as informações de produtos estocados por empresa ao relatório
-        report += "\nStocked products by company:\n" + company_list
-
-        return report
+                    company_inventory[company] = 1
+        return company_inventory
